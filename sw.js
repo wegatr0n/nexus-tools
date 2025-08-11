0a39c2dd257060d5dfde18da74f474d1062ebbaf
wegatr0n <wegahta.tewolde@gmail.com>
2025-08-10 21:56:53 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,7 +15,13 @@ const filesToCache = [
   '/apps/markdown.html',
   '/apps/base64.html',
   '/apps/hash.html',
+  '/apps/timestamp.html',
+  '/apps/bmi.html',
+  '/apps/lorem.html',
   '/apps/index.html',
+  '/timestamp.html',
+  '/bmi.html',
+  '/lorem.html',
   'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js',
   'https://cdn.jsdelivr.net/npm/marked/marked.min.js'
